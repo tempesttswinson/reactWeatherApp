966bcd6c1e5c562b76ad7e9fd69cd5d5f7cbab27
tempesttswinson <tempestt.swinson@gmail.com>
2020-04-23 17:29:53 -0400

    beginning stages without errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Input from "./components/input";
+import UserInput from "./components/UserInput";
 import Weather from "./components/weather";
 import time from "./components/time";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <Input />
-    </div>
+  return ( <
+    UserInput / >
+
   );
 }
---
+++ src/components/UserInput.js
@@ -0,0 +1,19 @@
+import React, {
+  Component,
+  useState,
+  useEffect
+} from "react";
+// import Button from "react-bootstrap/Button";
+// import Input from "react-bootstrap/Input";
+
+
+export default function UserInput() {
+  return ( <
+    div >
+    <
+    input placeholder = "Enter Zip Code" / >
+    <
+    button type = "submit" > Submit < /button>  <
+    /div>
+  )
+}
